f0b6dbd7acac394be5a5b40349fdb35bd5a6d0f6
steven112163 <steven112163@gmail.com>
2021-01-13 22:24:38 +0800

    Rewrite first result, but aggregation still has bugs

--- utils/aggregator.py
+++ utils/aggregator.py
@@ -1,10 +1,13 @@
 import sys
 import os
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
 import numpy as np
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 from argparse import ArgumentParser, Namespace
-from math import ceil
+from math import ceil, floor, sqrt
+from random import sample
+from typing import Dict
 
 
 def aggregate(dir_name: str, num_of_pkt: int) -> None:
@@ -34,120 +37,94 @@ def aggregate(dir_name: str, num_of_pkt: int) -> None:
                     if serial_number == '0':
                         zero_serial_number_csv[host] = read_csv(file.path)
 
-    # Plot the serial_number = 0 picture
-    for key in zero_serial_number_csv.keys():
-        if zero_serial_number_csv.get(key) is not None:
-
-            result = zero_serial_number_csv[key]
-
-            # plot
-            fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
-            # Occurrences vs. Route
-            count = result.groupby(['IDs'], as_index=False).count()
-            count.rename(columns={'Num_of_switch': 'Count'}, inplace=True)
-            rects = ax1.bar(count['IDs'], count['Count'])
-            for rect in rects:
-                height = rect.get_height()
-                ax1.annotate(f'{height}',
-                             xy=(rect.get_x() + rect.get_width() / 2, height),
-                             xytext=(0, 3),  # 3 points vertical offset
-                             textcoords='offset points',
-                             ha='center',
-                             va='bottom')
-
-            ax1.set_yticks(range(0, ceil(max(count['Count'])) + 2))
-            ax1.set_ylabel('The number of occurrences')
-            ax1.set_xlabel('Route')
-            ax1.set_title('Occurrences vs. Route')
-
-            # Occurrence of the route vs. Time
-            result.sort_values(by=['Time'], inplace=True)
-            minimum = min(result['Time'])
-            x_coord = np.arange(minimum, max(result['Time']), 0.001, dtype=float)
-            for i in result['IDs'].unique():
-                selected = result[result['IDs'] == i]
-                y_coord = np.zeros(x_coord.shape, dtype=int)
-                for t in selected['Time']:
-                    y_coord[int((t - minimum) / 0.001)] = 1
-                ax2.plot(x_coord, y_coord, label=f'{i}')
-            ax2.legend()
-            ax2.set_yticks([0, 1])
-            ax2.set_yticklabels(['Not', 'Appear'])
-            ax2.set_ylabel('Appear or not')
-            ax2.set_xlabel('Time (s)')
-            ax2.set_title('Occurrences of the route vs. Time')
-
-            fig.tight_layout()
-            fig.canvas.set_window_title(f'{key}')
-            plt.show()
-
-    # Output aggregation file and plot the aggregation
-    for key in aggregation_result.keys():
-        if aggregation_result.get(key) is not None:
-
-            result = aggregation_result[key]
-
-            del result['Time']
-            # Write the result to a file
-            info_log('Record the result')
-            result.to_csv(f'{dir_name}/{key}_aggregation.csv', index=False)
-
-            # Plot the aggregation
-            info_log('Draw aggregation')
-            num_of_rows = float(len(result.index)) / num_of_pkt
-            count = result.groupby(['IDs'], as_index=False).count()
-            count.rename(columns={'Num_of_switch': 'Count'}, inplace=True)
-            count['Count'] = count['Count'] / num_of_rows
-            fig, ax = plt.subplots()
-            rects = ax.bar(count['IDs'], count['Count'])
-            for rect in rects:
-                height = rect.get_height()
-                ax.annotate(f'{height:.3f}',
-                            xy=(rect.get_x() + rect.get_width() / 2, height),
-                            xytext=(0, 3),  # 3 points vertical offset
-                            textcoords="offset points",
-                            ha='center', va='bottom')
-            ax.set_yticks(range(0, ceil(max(count['Count'])) + 1))
-            ax.set_ylabel('Average number of occurrences')
-            ax.set_xlabel('Route')
-            ax.set_title('Average occurrences vs. Route')
-            fig.tight_layout()
-            plt.show()
+    # Draw graphs
+    draw_graphs(aggregation_result, zero_serial_number_csv, num_of_pkt)
 
 
-'''
-def aggregate(dir_name: str, num_of_pkt: int) -> None:
+def draw_graphs(aggregation_result: Dict[str, DataFrame], zero_serial_number_csv: Dict[str, DataFrame],
+                num_of_pkt: int) -> None:
     """
-    Aggregate the csv results in the given directory
-    :param dir_name: name of the given directory
-    :param num_of_pkt: number of packets sent in each round
+    Draw the graphs
+    :param aggregation_result: aggregation result of each host
+    :param zero_serial_number_csv: first result of each host
+    :param num_of_pkt: number of packets in each round
     :return: None
     """
-    # Aggregate all csv
-    result = None
-    with os.scandir(dir_name) as directory:
-        for file in directory:
-            if file.path.endswith('.csv') and file.is_file():
-                if result is None:
-                    result = read_csv(file.path)
-                else:
-                    part = read_csv(file.path)
-                    result = result.append(part, ignore_index=True)
+    # Get number of rows/columns for display
+    num_of_hosts = len(zero_serial_number_csv.keys())
+    num_of_rows_or_cols = int(floor(sqrt(num_of_hosts)))
 
-    if result is not None:
-        del result['Time']
+    # Get keys
+    if num_of_rows_or_cols > 3:
+        keys = sample(list(zero_serial_number_csv), k=9)
+        num_of_rows_or_cols = 3
+    else:
+        keys = sample(list(zero_serial_number_csv), k=num_of_rows_or_cols ** 2)
+    keys.sort()
+    keys.sort(key=len)
 
-        # Write the result to a file
-        info_log('Record the result')
-        result.to_csv(f'{dir_name}/aggregation.csv', index=False)
+    # Plot the serial_number = 0 picture
+    fig = plt.figure()
+    outer = gs.GridSpec(num_of_rows_or_cols, num_of_rows_or_cols)
+    for idx, key in enumerate(keys):
+        inner = gs.GridSpecFromSubplotSpec(1, 2, subplot_spec=outer[idx])
+        result = zero_serial_number_csv[key]
 
-        # Plot the aggregation
-        info_log('Draw')
-        num_of_rows = float(len(result.index)) / num_of_pkt
+        # Occurrences vs. Route
+        ax = plt.Subplot(fig, inner[0])
         count = result.groupby(['IDs'], as_index=False).count()
         count.rename(columns={'Num_of_switch': 'Count'}, inplace=True)
-        count['Count'] = count['Count'] / num_of_rows
-        fig, ax = plt.subplots()
+        rects = ax.bar(count['IDs'], count['Count'])
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate(f'{height}',
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords='offset points',
+                        ha='center',
+                        va='bottom')
+
+        ax.set_yticks(range(0, ceil(max(count['Count'])) + 3))
+        ax.set_ylabel('Occurrences')
+        ax.set_xlabel('Route')
+        ax.set_title(f'{key}')
+        fig.add_subplot(ax)
+
+        # Occurrence of the route vs. Time
+        ax = plt.Subplot(fig, inner[1])
+        result.sort_values(by=['Time'], inplace=True)
+        minimum = min(result['Time'])
+        x_coord = np.arange(minimum, max(result['Time']), 0.001, dtype=float)
+        for i in result['IDs'].unique():
+            selected = result[result['IDs'] == i]
+            y_coord = np.zeros(x_coord.shape, dtype=int)
+            for t in selected['Time']:
+                y_coord[int((t - minimum) / 0.001)] = 1
+            ax.plot(x_coord, y_coord, label=f'{i}')
+        ax.legend()
+        ax.set_yticks([0, 1])
+        ax.set_yticklabels(['F', 'T'])
+        ax.set_ylabel('Appear or not')
+        ax.set_xlabel('Time (s)')
+        ax.set_title(f'{key}')
+        fig.add_subplot(ax)
+        fig.tight_layout()
+    fig.tight_layout()
+    fig.canvas.set_window_title('Results of first test')
+
+    # Plot aggregation result
+    fig = plt.figure()
+    grid = gs.GridSpec(num_of_rows_or_cols, num_of_rows_or_cols)
+    for idx, key in enumerate(keys):
+        result = aggregation_result[key]
+        del result['Time']
+
+        # Plot the aggregation
+        num_of_tests = float(len(result.index)) / num_of_pkt
+        count = result.groupby(['IDs'], as_index=False).count()
+        count.rename(columns={'Num_of_switch': 'Count'}, inplace=True)
+        count['Count'] = count['Count'] / num_of_tests
+        ax = plt.Subplot(fig, grid[idx])
         rects = ax.bar(count['IDs'], count['Count'])
         for rect in rects:
             height = rect.get_height()
@@ -159,10 +136,11 @@ def aggregate(dir_name: str, num_of_pkt: int) -> None:
         ax.set_yticks(range(0, ceil(max(count['Count'])) + 1))
         ax.set_ylabel('Average number of occurrences')
         ax.set_xlabel('Route')
-        ax.set_title('Average occurrences vs. Route')
-        fig.tight_layout()
-        plt.show()
-'''
+        ax.set_title(f'{key}Average occurrences vs. Route')
+    fig.tight_layout()
+    fig.canvas.set_window_title('Aggregation result')
+
+    plt.show()
 
 
 def info_log(log: str) -> None:
